a7ba15d39bc611125bf59ebc26b38c94dbc3a51a
Danilofranc <danilofranco2011@hotmail.com>
2024-11-22 16:41:00 -0300

    commitSeçãoAula0

--- dashboard.js
+++ dashboard.js
@@ -43,3 +43,159 @@ document.getElementById('logoutButton').addEventListener('click', () => {
     window.location.href = 'index.html'; // Redireciona para login
 });
 
+
+// página de aulas \/
+
+const AlinSubtitles0=document.getElementById("AlinSubtitles0")
+const boxClassroom=document.getElementById("boxClassroom")
+
+AlinSubtitles0.addEventListener("click",()=>{
+
+    while (boxClassroom.firstChild) {
+        boxClassroom.removeChild(boxClassroom.firstChild);
+    }
+
+
+    const titleAula=document.createElement("div")
+    titleAula.setAttribute("class", "titleAula")
+    titleAula.innerHTML ="Aula 1 - Abertura"
+    boxClassroom.prepend(titleAula)
+
+    const videoAula=document.createElement("video")
+    videoAula.setAttribute("class","videoAula")
+    videoAula.setAttribute("controls","")
+
+    const aula=document.createElement("source")
+    aula.setAttribute("src", "videos/aula 1 - Abertura.mp4")
+    aula.setAttribute("type", "video/mp4")    
+    videoAula.appendChild(aula)
+    boxClassroom.appendChild(videoAula)
+
+       
+    const AlinBotApostilas=document.createElement("div")
+    AlinBotApostilas.setAttribute("class","AlinBotApostilas")
+    boxClassroom.appendChild(AlinBotApostilas)
+
+    const buttonExercicies=document.createElement("button")
+    buttonExercicies.setAttribute("id","buttonExercicies")
+    buttonExercicies.setAttribute("class","BotãoExercicio clickedButton")
+    buttonExercicies.innerHTML="EXERCÍCIO"
+    AlinBotApostilas.appendChild(buttonExercicies)
+
+    const buttonApostila=document.createElement("button")
+    buttonApostila.setAttribute("id","buttonApostila")
+    buttonApostila.setAttribute("class","BotãoApostila")
+    buttonApostila.innerHTML="APOSTILA"
+    AlinBotApostilas.appendChild(buttonApostila)
+
+    const ExerciciesBox=document.createElement("div")
+    ExerciciesBox.setAttribute("class","ExerciciesBox")
+    boxClassroom.appendChild(ExerciciesBox)
+
+    const lineExerciciesBox=document.createElement("div")
+    lineExerciciesBox.setAttribute("class","lineExerciciesBox")
+    ExerciciesBox.prepend(lineExerciciesBox)
+
+    const buttonIniciarExercicio=document.createElement("button")
+    buttonIniciarExercicio.setAttribute("class","buttonIniciarExercicio")
+    buttonIniciarExercicio.innerHTML="Iniciar Exercício"
+    ExerciciesBox.appendChild(buttonIniciarExercicio)
+
+    const lineExerciciesBox1=document.createElement("div")
+    lineExerciciesBox1.setAttribute("class","lineExerciciesBox1")
+    ExerciciesBox.appendChild(lineExerciciesBox1)
+
+
+    buttonApostila.addEventListener('click', function() {
+        
+        buttonApostila.classList.add('clickedButton');  
+        buttonApostila.classList.remove('unclickedButton');  
+        
+        buttonExercicies.classList.remove('clickedButton'); 
+        buttonExercicies.classList.add('unclickedButton'); 
+        
+        while (ExerciciesBox.firstChild) {
+            ExerciciesBox.removeChild(ExerciciesBox.firstChild);
+        }
+
+
+
+        // <!-- <div class="lineExerciciesBox"></div>
+        //             <div class="titleHandout">ACESSE O RESUMO DESTE MÓDULO.</div>
+        //             <button class="buttonIniciarExercicio1">Apostila</button>
+        //             <div class="lineExerciciesBox1"></div> -->
+
+        // const ExerciciesBox=document.createElement("div")
+        // ExerciciesBox.setAttribute("class","ExerciciesBox")
+        // boxClassroom.appendChild(ExerciciesBox)
+
+        const lineExerciciesBox=document.createElement("div")
+        lineExerciciesBox.setAttribute("class","lineExerciciesBox")
+        ExerciciesBox.prepend(lineExerciciesBox)
+
+        const titleHandout=document.createElement("div")
+        titleHandout.setAttribute("class","titleHandout")
+        titleHandout.innerHTML="ACESSE O RESUMO DESTE MÓDULO"
+        ExerciciesBox.appendChild(titleHandout)
+
+        const buttonIniciarExercicio1=document.createElement("button")
+        buttonIniciarExercicio1.setAttribute("class","buttonIniciarExercicio1")
+        buttonIniciarExercicio1.innerHTML="Apostila"
+        ExerciciesBox.appendChild(buttonIniciarExercicio1)
+
+        const lineExerciciesBox1=document.createElement("div")
+        lineExerciciesBox1.setAttribute("class","lineExerciciesBox1")
+        ExerciciesBox.appendChild(lineExerciciesBox1)
+
+
+
+
+    });
+
+    buttonExercicies.addEventListener('click', function() {
+        
+        buttonExercicies.classList.add('clickedButton');  
+        buttonExercicies.classList.remove('unclickedButton');  
+        
+        buttonApostila.classList.remove('clickedButton');  
+        buttonApostila.classList.add('unclickedButton');  
+
+
+        while (ExerciciesBox.firstChild) {
+            ExerciciesBox.removeChild(ExerciciesBox.firstChild);
+        }
+
+
+
+        // const ExerciciesBox=document.createElement("div")
+        // ExerciciesBox.setAttribute("class","ExerciciesBox")
+        // boxClassroom.appendChild(ExerciciesBox)
+
+        const lineExerciciesBox=document.createElement("div")
+        lineExerciciesBox.setAttribute("class","lineExerciciesBox")
+        ExerciciesBox.prepend(lineExerciciesBox)
+
+        const buttonIniciarExercicio=document.createElement("button")
+        buttonIniciarExercicio.setAttribute("class","buttonIniciarExercicio")
+        buttonIniciarExercicio.innerHTML="Iniciar Exercício"
+        ExerciciesBox.appendChild(buttonIniciarExercicio)
+
+        const lineExerciciesBox1=document.createElement("div")
+        lineExerciciesBox1.setAttribute("class","lineExerciciesBox1")
+        ExerciciesBox.appendChild(lineExerciciesBox1)
+
+
+
+    });
+
+    
+    
+
+    
+
+})
+
+
+
+
+
